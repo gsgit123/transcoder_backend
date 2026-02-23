effdbc1c27bcce6974ac554e1ca18e1e269622f7
gsgit123 <gauravexp123@gmail.com>
2026-02-23 23:40:49 +0530

    updated package.json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: ".env.local" });
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const ffmpeg = require('fluent-ffmpeg');
